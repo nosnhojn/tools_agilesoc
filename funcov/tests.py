16cd568bf8dcc367963d3515cbe539e02461bd63
nosnhojn <nosnhojn@gmail.com>
2015-09-17 00:55:02 -0600

    updates to the editor

--- funcov/tests.py
+++ funcov/tests.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 from unittest.mock import patch
 from django.core.urlresolvers import reverse
 from registration.forms import RegistrationForm
-import django
+from django.http import HttpRequest
+from django.http import HttpResponse
 
 import funcov.views
 
@@ -34,9 +35,9 @@ class userTests(TestCase):
     self.assertEquals(response.context['h1'], 'FunkOv')
     self.assertEquals(response.context['h2'], 'Pick an interface to get started')
     self.assertEquals(response.context['h3'], '')
-    self.assertEquals(response.context['buttons']['AHB'], reverse('index'))
-    self.assertEquals(response.context['buttons']['APB'], reverse('index'))
-    self.assertEquals(response.context['buttons']['AXI4-Stream'], reverse('index'))
+    self.assertEquals(response.context['buttons']['AHB']['type'], 'ahb')
+    self.assertEquals(response.context['buttons']['APB']['type'], 'apb')
+    self.assertEquals(response.context['buttons']['AXI4-Stream']['type'], 'axi4stream')
 
   def testLoginRedirect(self):
     self.createUser()
@@ -75,12 +76,7 @@ class userTests(TestCase):
 
 
 
-from django.http import HttpRequest
-from django.http import HttpResponse
-class viewTests(TestCase):
-  def __init__(self, *args, **kwargs):
-    super(viewTests, self).__init__(*args, **kwargs)
-
+class generalViewTests(TestCase):
   def testUrlTranslationViewExists(self):
     response = self.client.get('/FuNcOv/')
     self.assertEqual(response.status_code, 200)
@@ -89,6 +85,13 @@ class viewTests(TestCase):
     response = self.client.get(reverse('index'))
     self.assertEqual(response.status_code, 200)
 
+  def testEditorRedirectsWithoutLogin(self):
+    response = self.client.post(reverse('editor'))
+    self.assertEqual(response.status_code, 302)
+
+
+
+class indexViewTests(TestCase):
   @patch('funcov.views.render') # mocked relative the to the module using it, not the definition
   def testIndexRendersIndexHtml(self, mock_render):
     request = HttpRequest()
@@ -102,17 +105,60 @@ class viewTests(TestCase):
     response = self.client.get(reverse('auth_login'))
     self.assertEqual(response.status_code, 200)
 
+
+
+class editorViewTests(TestCase):
+  def setUp(self):
+    up = UserProfile()
+    up.user = User.objects.create_user('a', 'b', 'c')
+    up.save()
+    self.client.login(username='a', password='c')
+
   def testEditorViewExists(self):
     response = self.client.get(reverse('editor'))
     self.assertEqual(response.status_code, 200)
 
-  @patch('funcov.views.render') # mocked relative the to the module using it, not the definition
-  def testEditorRendersEditorHtml(self, mock_render):
-    request = HttpRequest()
-    funcov.views.editor(request)
+  def testEditorRedirectForUndefinedType(self):
+    response = self.client.post(reverse('editor'), { 'type':'bagels' })
+    self.assertEqual(response.status_code, 302)
+
+  @patch('funcov.views.HttpResponseRedirect', return_value=HttpResponse())
+  def testEditorRedirectToIndex(self, mock_HttpResponseRedirect):
+    self.client.post(reverse('editor'), { 'type':'bagels' })
+    mock_HttpResponseRedirect.assert_called_with(reverse('index'))
+
+  @patch('funcov.views.render', return_value=HttpResponse())
+  def testRendersAhb(self, mock_render):
+    self.client.post(reverse('editor'), { 'type':'ahb' })
     args, kwargs = mock_render.call_args
-    self.assertEqual(args[0], request)
     self.assertEqual(args[1], 'funcov/editor.html')
+
+  def testAhbContext(self):
+    response = self.client.post(reverse('editor'), { 'type':'ahb' })
+    self.assertEqual(response.context['type'], 'AHB')
+    self.assertTrue(len(response.context['covergroups']) > 0)
+
+  @patch('funcov.views.render', return_value=HttpResponse())
+  def testRendersApb(self, mock_render):
+    self.client.post(reverse('editor'), { 'type':'apb' })
+    args, kwargs = mock_render.call_args
+    self.assertEqual(args[1], 'funcov/editor.html')
+
+  def testApbContext(self):
+    response = self.client.post(reverse('editor'), { 'type':'apb' })
+    self.assertEqual(response.context['type'], 'APB')
+    self.assertTrue(len(response.context['covergroups']) > 0)
+
+  @patch('funcov.views.render', return_value=HttpResponse())
+  def testRendersStreamAxi4(self, mock_render):
+    self.client.post(reverse('editor'), { 'type':'axi4stream' })
+    args, kwargs = mock_render.call_args
+    self.assertEqual(args[1], 'funcov/editor.html')
+
+  def testAxi4StreamContext(self):
+    response = self.client.post(reverse('editor'), { 'type':'axi4stream' })
+    self.assertEqual(response.context['type'], 'AXI-4 Streaming')
+    self.assertTrue(len(response.context['covergroups']) > 0)
 
 
 
--- funcov/views.py
+++ funcov/views.py
@@ -1,37 +1,69 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from registration.forms import RegistrationForm
 from django.core.urlresolvers import reverse
 
 
 def index(request):
+    context = {}
     if request.user.is_authenticated():
-      context_dict = {
-                       'title': request.user.username,
-                       'h1': 'FunkOv',
-                       'h2': 'Pick an interface to get started',
-                       'h3': '',
-                       'buttons' : {
-                                     'AHB':reverse('index'),
-                                     'APB':reverse('index'),
-                                     'AXI4-Stream':reverse('index'),
-                                   },
-                     }
+      context = {
+                  'title': request.user.username,
+                  'h1': 'FunkOv',
+                  'h2': 'Pick an interface to get started',
+                  'h3': '',
+                  'buttons' : {
+                                'AHB': {
+                                         'type' : 'ahb',
+                                       },
+                                'APB': {
+                                         'type' : 'apb',
+                                       },
+                                'AXI4-Stream': {
+                                         'type' : 'axi4stream',
+                                       },
+                              },
+                }
 
     else:
-      context_dict = {
-                       'title': 'FunkOv',
-                       'h1': 'FunkOv',
-                       'h2': 'Funktional Coverage Made Easy',
-                       'h3': 'For design and verification engineers that care',
-                       'buttons' : {
-                                     'Register':'/accounts/register/',
-                                     'Login':'/accounts/login/',
-                                   },
-                     }
+      context = {
+                  'title': 'FunkOv',
+                  'h1': 'FunkOv',
+                  'h2': 'Funktional Coverage Made Easy',
+                  'h3': 'For design and verification engineers that care',
+                  'buttons' : {
+                                'Register':'/accounts/register/',
+                                'Login':'/accounts/login/',
+                              },
+                }
 
-    return render(request, 'funcov/index.html', context_dict)
+    return render(request, 'funcov/index.html', context)
 
+
+@login_required
 def editor(request):
-    return render(request, 'funcov/editor.html')
+    context = {}
+    if request.method == 'POST':
+      type = request.POST.get('type')
+      if type == 'ahb':
+        context['type'] = "AHB"
+        context['covergroups'] = {
+                                   'name':'not empty',
+                                 }
+      elif type == 'apb':
+        context['type'] = "APB"
+        context['covergroups'] = {
+                                   'name':'not empty',
+                                 }
+      elif type == 'axi4stream':
+        context['type'] = "AXI-4 Streaming"
+        context['covergroups'] = {
+                                   'name':'not empty',
+                                 }
+      else:
+        return HttpResponseRedirect(reverse('index'))
+
+    return render(request, 'funcov/editor.html', context)
