b233424de72bb4d4327ac51a44f7b6095cb708a8
nosnhojn <nosnhojn@gmail.com>
2015-10-08 01:02:06 -0600

    checkpoint

--- funcov/forms.py
+++ funcov/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class ParameterForm(forms.Form):
   enable = forms.BooleanField(initial=True)
   name = forms.CharField(widget = forms.HiddenInput())
-  select = forms.ChoiceField()
+  select = forms.ChoiceField(required=False)
